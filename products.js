51c89697e2cc2480857b4210c48873fe98dedc15
Bennyhousemedia <110995284+Bennyhousemedia@users.noreply.github.com>
2024-01-29 21:18:15 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,57 @@
+const products = [
+    {
+      id: 0,
+      name: "Elegance",
+      price: 40.99,
+      instock: 10,
+      description:
+        "A mixed color sneaker that combines white with a touch of gray to give an elegance look",
+      imgSrc: "./img/s1.jpg",
+    },
+
+    {id: 1,
+      name: "Finnese",
+      price: 100.99,
+      instock: 50,
+      description:
+        "A pink sneaker that combines black with a dominate pink to give an elegance look",
+      imgSrc: "./img/s2.jpg",
+    },
+
+    {id: 2,
+      name: "Axxel",
+      price: 56.10,
+      instock: 10,
+      description:
+        "Female black sneaker that combines class and elegance",
+      imgSrc: "./img/s3.jpg",
+    },
+
+    {id: 3,
+      name: "Wheela Pro",
+      price: 44.10,
+      instock: 10,
+      description:
+        "A Unisex white kicks that blends accross all occassions",
+      imgSrc: "./img/s4.jpg",
+    },
+
+    {id: 4,
+      name: "Condo",
+      price: 56.10,
+      instock: 4,
+      description:
+        "A concert light brown sneaker that oozes a touch of modernity",
+      imgSrc: "./img/s5.jpg",
+    },
+
+    {id: 5,
+      name: "Tzal",
+      price: 77.10,
+      instock: 10,
+      description:
+        "Highfighger Sneaker that serves all purpose as long as its worn correctly",
+      imgSrc: "./img/s6.jpg",
+    },
+
+];
